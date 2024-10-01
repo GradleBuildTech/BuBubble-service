aa360a59d705d9383c1b24b0faeb22b4bca63d01
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2024-10-02 00:08:56 +0700

    🗑️ chore: add example module

--- app/src/main/java/com/example/bububleservice/view/BubbleView.kt
+++ app/src/main/java/com/example/bububleservice/view/BubbleView.kt
@@ -1,4 +1,18 @@
 package com.example.bububleservice.view
 
-class BubbleView {
+import android.content.Context
+import com.example.bububleservice.event.BubbleListener
+import com.example.bububleservice.view.layout.BubbleInitialization
+import com.example.bububleservice.view.layout.BubbleLayout
+
+class BubbleView(
+    context: Context,
+    containCompose: Boolean,
+    private val forceDragging: Boolean = false,
+    private val listener: BubbleListener? = null
+) : BubbleInitialization(
+    context = context,
+    containCompose = containCompose,
+    root = BubbleLayout(context)
+) {
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "bububle service"
 include(":app")
- 
+include(":example")
